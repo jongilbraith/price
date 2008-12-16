bc72ac4e078e7023a9643335138f30380e8fd608
Jon Gilbraith <jon@professionalnerd.com>
2008-12-16 20:39:00 +0000

    Added comparison and arithmetic.

--- lib/price.rb
+++ lib/price.rb
@@ -9,15 +9,56 @@ class Price
   attr_accessor :tax
   
   def initialize(cents, currency, &block)
-    @money = Money.new(cents, currency)
-    @tax   = Money.new(0, currency)
+    @currency = currency
+    @money    = Money.new(cents, @currency)
+    @tax      = Money.new(0, @currency)
     yield self if block_given?
+  end
+  
+  def tax_cents=(cents)
+    @tax = Money.new(cents, @currency)
   end
   
   def take_tax_from_money
     tmp    = @money.cents.to_f / (1000 + @@tax_rate)
-    @money = Money.new(tmp * 1000, currency)
-    @tax   = Money.new(tmp * @@tax_rate, currency)
+    @money = Money.new(tmp * 1000, @currency)
+    @tax   = Money.new(tmp * @@tax_rate, @currency)
+  end
+
+  def ==(other_price)
+    raise "Cannot perform comparison between prices of different currency" if self.currency == other_price.currency
+    self.with_tax == other_price.with_tax
+  end
+  
+  def <=>(other_price)
+    raise "Cannot perform comparison between prices of different currency" if self.currency == other_price.currency
+    self.with_tax <=> other_price.with_tax
+  end
+  
+  def +(other_price)
+    raise "Cannot perform arithmetic between prices of different currency" if self.currency == other_price.currency
+    Price.new(self.money.cents + other_price.money.cents, @currency) do |price|
+      price.tax_cents = self.tax.cents + other_price.tax.cents
+    end
+  end
+
+  def -(other_price)
+    raise "Cannot perform arithmetic between prices of different currency" if self.currency == other_price.currency
+    Price.new(self.money.cents - other_price.money.cents, @currency) do |price|
+      price.tax_cents = self.tax.cents - other_price.tax.cents
+    end
+  end
+
+  def *(fixnum)
+    Price.new(self.money.cents * fixnum, @currency) do |price|
+      price.tax_cents = self.tax.cents.to_f * fixnum
+    end
+  end
+
+  def /(fixnum)
+    Price.new(self.money.cents / fixnum, @currency) do |price|
+      price.tax_cents = self.tax.cents.to_f / fixnum
+    end
   end
   
   def tax_rate
