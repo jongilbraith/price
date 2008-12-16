5adeb3dc100424a4cb62dab7900ecbd50c38a72d
Jon Gilbraith <jon@professionalnerd.com>
2008-12-16 20:15:56 +0000

    Rethought my approach to this - no longer recording the taxable flag and storing the tax internally rather than calculating it on request. Initialize now takes a block with is nicer syntax really.

--- lib/price.rb
+++ lib/price.rb
@@ -6,29 +6,20 @@ class Price
   cattr_accessor :tax_rate
   
   attr_accessor :money
+  attr_accessor :tax
   
-  attr_accessor :taxable
+  def initialize(cents, currency, &block)
+    @money = Money.new(cents, currency)
+    @tax   = Money.new(0, currency)
+    yield self if block_given?
+  end
   
-  def initialize(cents, currency, taxable = true, tax_inclusive = false)
-    @taxable  = taxable
-    
-    raise "If something isn't taxable, you shouldn't be passing a tax inclusive price to it!" if taxable == false && tax_inclusive == true
+  def take_tax_from_money
+    tmp    = @money.cents.to_f
+    @money = Money.new((tmp / (1000 + @@tax_rate) * 1000), currency)
+    @tax   = Money.new((tmp / (1000 + @@tax_rate) * @@tax_rate), currency)
+  end
 
-    if tax_inclusive == true
-      @money = Money.new((cents.to_f / (1000 + @@tax_rate) * 1000), currency)
-    else
-      @money = Money.new(cents, currency)
-    end
-  end
-  
-  def tax
-    if self.taxable == true
-      Money.new((self.money.cents.to_f / 1000 * @@tax_rate), self.money.currency)
-    else
-      Money.new(0, self.money.currency)
-    end
-  end
-  
   def tax_rate
     @@tax_rate.to_f / 10
   end
@@ -40,8 +31,6 @@ class Price
   def without_tax
     self.money
   end
-  
-  alias :taxable? :taxable
   
   def method_missing(method, *args)
     if self.money.send(:respond_to?, method)
